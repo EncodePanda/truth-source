eb2b872a5501d6b61c4c31d4f1acb7e7cb44f66d
Pawel Szulc <paul.szulc@gmail.com>
2018-11-22 21:04:56 +0100

    Read markdown file and return its represenatation

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,27 @@
 module Main where
 
+import Text.Pandoc.Options
+import Text.Pandoc.Class
+import Text.Pandoc.Readers.Markdown
+import Text.Pandoc.Definition
+import Text.Pandoc.Error
+
+import Control.Monad.Except
 import Options.Applicative
 import qualified Config as C
 import qualified Truth as T
 
 main :: IO ()
-main = execParser opts >>= T.generate
+main = do
+  config <- execParser opts
+  pandocErr <- runIO $ T.generate config
+  handle pandocErr
   where
     opts = info (C.parser <**> helper)
       ( fullDesc
      <> progDesc "program desc"
      <> header "header" )
+
+    handle (Right(pandoc)) = putStrLn $ show pandoc
+    handle (Left(error)) = putStrLn $ show error
 
--- app/Truth.hs
+++ app/Truth.hs
@@ -1,7 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Truth where
 
+import Data.Text (Text, pack)
+import Text.Pandoc.Options
+import Text.Pandoc.Class
+import Text.Pandoc.Readers.Markdown
+import Text.Pandoc.Definition
 import Control.Monad.IO.Class
 import Config
 
-generate :: MonadIO m => Config -> m ()
-generate config = liftIO $ putStrLn $ show config
+generate :: (PandocMonad m, MonadIO m) => Config -> m Pandoc
+generate config = do
+  features <- liftIO $ readFile $ _featuresFile config
+  readMarkdown def (pack features)
