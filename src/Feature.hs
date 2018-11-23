f575954b02fdba746eba9e8648b40299945a0489
Pawel Szulc <paul.szulc@gmail.com>
2018-11-23 14:54:20 +0100

    Init CombineSpec + move Truth.hs

--- src/Feature.hs
+++ src/Feature.hs
@@ -1,19 +1,22 @@
 module Feature where
 
 newtype Features = Features [Feature]
+  deriving (Show, Eq)
 
 data Feature = Feature { _name :: String
                        , _userStories :: [UserStory]
-                       }
+                       } deriving (Show, Eq)
 data UserStory = UserStory { _userStoryDesc :: String
                            , _criteria :: [Criteria]
-                           }
+                           } deriving (Show, Eq)
 
 data Criteria = Criteria { _criteriaName :: String
                          , _status :: Status
                          , _steps :: [Step]
-                         }
+                         } deriving (Show, Eq)
 
 newtype Step = Step String
+  deriving (Show, Eq)
 
 data Status = Done | NotDone
+  deriving (Show, Eq)
