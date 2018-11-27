477db1d6b3359f7790f9e1179ca9a91aaa892a7a
Dominik Zajkowski <dzajkowski@invisibl.es>
2018-11-27 12:51:23 +0100

    CORE-1477 Added steps to features list.

--- src/Feature.hs
+++ src/Feature.hs
@@ -11,6 +11,7 @@ import Text.Pandoc.Class as PC
 import Text.Pandoc.Readers.Markdown
 import Text.Pandoc.Definition
 import Data.List
+import Control.Monad
 
 data Features = Features { _features :: [Feature]}
   deriving (Show, Eq, Generic)
@@ -43,33 +44,77 @@ makeLenses ''Criteria
 allCriteria :: Traversal' Features [Criteria]
 allCriteria = features.traverse.userStories.traverse.criteria
 
-extractFeaturesFromPandoc (Pandoc meta heads) = extractFeatures heads
+extractFeaturesFromPandoc :: Pandoc -> Features
+extractFeaturesFromPandoc (Pandoc meta heads) = buildFeatureTrees (getMainnetFeatureReqs heads)
   where
-    extractFeatures :: [Block] -> Features
-    extractFeatures l = buildFeatureTrees (takeWhile lowerLevel (tail (dropWhile (\v -> (not (featureReq v))) l)))
+    getMainnetFeatureReqs :: [Block] -> [Block]
+    getMainnetFeatureReqs l = takeWhile lowerLevel (tail (dropWhile (\v -> (not (featureReq v))) l))
+
+    wrapUpFeatures :: ([Feature], Feature) -> Features
+    wrapUpFeatures (acc, last) = Features (acc ++ [last])
+
+    buildFeatureTrees :: [Block] -> Features
+    buildFeatureTrees l = wrapUpFeatures (foldl mapBlockToFeature ([], makeFeature (head l)) (tail l))
+
     featureReq :: Block -> Bool
     featureReq (Header 1 _ [Str "Mainnet",Space,Str "Feature",Space,Str "Requirements"]) = True
     featureReq _ = False
+
+    lowerLevel :: Block -> Bool
     lowerLevel (Header 1 _ _) = False
     lowerLevel _ = True
-    wrapUpFeatures (acc, last) = Features (acc ++ [last])
-    buildFeatureTrees l =  wrapUpFeatures (foldl mapper ([], makeFeature (head l)) (tail l))
-    makeFeature (Header 2 _ l) = Feature (printInline l) []
-    makeUserStory (Header 3 _ l) = UserStory (printInline l) []
-    makeCriteria (Header 4 _ l) = Criteria (printInline l) "not available" Missing []
-    addNameToCriteriaInUserStory (UserStory storyName crits) name = (UserStory storyName ((init crits) ++ [(addNameToCriteria (last crits) name)]))
-    appendCriteria (UserStory n crs) c = UserStory n (crs ++ [c])
+
+makeFeature :: Block -> Feature
+makeFeature (Header 2 _ l) = Feature (printInline l) []
+
+makeUserStory :: Block -> UserStory
+makeUserStory (Header 3 _ l) = UserStory (printInline l) []
+
+makeCriteria :: Block -> Criteria
+makeCriteria (Header 4 _ l) = Criteria (printInline l) "not available" Missing []
+
+mapBlockToFeature :: ([Feature], Feature) -> Block -> ([Feature], Feature)
+mapBlockToFeature (l, c) h@(Header 2 _ _) = (l ++ [c], makeFeature h)
+mapBlockToFeature (l, (Feature n []) ) h@(Header 3 _ _) = (l, Feature n [makeUserStory h])
+mapBlockToFeature (l, (Feature n ucs) ) h@(Header 3 _ _) = (l, Feature n (ucs ++ [makeUserStory h]))
+mapBlockToFeature (l, (Feature n ucs) ) h@(Header 4 _ _) = (l, Feature n ((init ucs) ++ [(appendCriteria (last ucs) (makeCriteria h))]))
+mapBlockToFeature (l, (Feature n ucs) ) h@(Header 5 _ (line@((Str "test:"):rest))) = (l, Feature n ((init ucs) ++ [(addNameToCriteriaInUserStory (last ucs) (printInline line))]))
+mapBlockToFeature (l, f) h@(Header 5 _ (line@((Str "steps:"):rest))) = (l, f)
+mapBlockToFeature (l, (Feature n ucs) ) (BulletList elems) = (l, Feature n ((init ucs) ++ [(addStepsToCriteriaInUserStory (last ucs) elems)]))
+mapBlockToFeature (l, f) _ = (l, f)
+
+appendCriteria :: UserStory -> Criteria -> UserStory
+appendCriteria (UserStory n crs) c = UserStory n (crs ++ [c])
+
+addNameToCriteriaInUserStory :: UserStory -> String -> UserStory
+addNameToCriteriaInUserStory (UserStory storyName crits) name = (UserStory storyName ((init crits) ++ [(addNameToCriteria (last crits) name)]))
+  where
+    addNameToCriteria :: Criteria -> String -> Criteria
     addNameToCriteria (Criteria cn tn st s) name = Criteria cn name st s
-    mapper (l, c) h@(Header 2 _ _) = (l ++ [c], makeFeature h)
-    mapper (l, (Feature n []) ) h@(Header 3 _ _) = (l, Feature n [makeUserStory h])
-    mapper (l, (Feature n ucs) ) h@(Header 3 _ _) = (l, Feature n (ucs ++ [makeUserStory h]))
-    mapper (l, (Feature n ucs) ) h@(Header 4 _ _) = (l, Feature n ((init ucs) ++ [(appendCriteria (last ucs) (makeCriteria h))]))
-    mapper (l, (Feature n ucs) ) h@(Header 5 _ (line@((Str "test:"):rest))) = (l, Feature n ((init ucs) ++ [(addNameToCriteriaInUserStory (last ucs) (printInline line))]))
-    mapper (l, f) h@(Header 5 _ (line@((Str "steps:"):rest))) = (l, f)
-    mapper (l, f) (BulletList _) = (l, f)
-    mapper (l, f) _ = (l, f)
-    toStringInline (Str s) = s
-    toStringInline Space = " "
-    toStringInline a = show a
-    printInline :: [Inline] -> String
-    printInline l = intercalate "" (map toStringInline l)
+
+addStepsToCriteriaInUserStory :: UserStory -> [[Block]] -> UserStory
+addStepsToCriteriaInUserStory (UserStory n crs) blocks = UserStory n ((init crs) ++ [addStepsToCriteria (last crs) (blocksToSteps blocks)])
+  where
+    addStepsToCriteria :: Criteria -> [Step] -> Criteria
+    addStepsToCriteria (Criteria cn tn st _) sts = Criteria cn tn st sts
+    blocksToSteps :: [[Block]] -> [Step]
+    blocksToSteps (a:rest) = (map Step (printBlocks a)) ++ (blocksToSteps rest)
+    blocksToSteps ([]) = []
+
+toStringInline :: Inline -> String
+toStringInline (Str s) = s
+toStringInline (Code _ s) = s
+toStringInline (Link _ ins t) = printInline ins
+toStringInline Space = " "
+toStringInline a = show a
+
+printInline :: [Inline] -> String
+printInline l = intercalate "" (map toStringInline l) 
+
+printBlock :: Block -> String
+printBlock (Plain inlines) = printInline inlines
+printBlock b = show b
+
+printBlocks :: [Block] -> [String]
+printBlocks (x:xs) = printBlock x : printBlocks xs
+printBlocks [] = []
--- test/FeaturesSpec.hs
+++ test/FeaturesSpec.hs
@@ -9,7 +9,7 @@ import Tests as T
 import FeaturesRaw as Raw
 import Control.Lens
 import Text.Pandoc.Options
-import Text.Pandoc.Class as PC
+import qualified Text.Pandoc.Class as PC
 import Text.Pandoc.Readers.Markdown
 import Text.Pandoc.Definition
 import Data.Text (pack)
@@ -31,11 +31,52 @@ spec = do
       Features {_features = [
         Feature {_featureName = "Network Launch", _userStories = [
           UserStory {_userStoryDesc = "UC: As a Coop SRE I want to launch a network", _criteria = [
-            Criteria {_criteriaName = "AC: A succesful genesis ceremony", _testName = "test: [todo: requires integration test]", _status = Missing, _steps = []},
-            Criteria {_criteriaName = "AC: A succesful genesis ceremony with read-only nodes joining", _testName = "test: [todo: requires integration test]", _status = Missing, _steps = []},
-            Criteria {_criteriaName = "AC: A NOT succesful genesis ceremony (not enough sigs)", _testName = "test: [todo: requires integration test]", _status = Missing, _steps = []},
-            Criteria {_criteriaName = "AC: A validator catching up after ceremony", _testName = "test: [todo: requires integration test]", _status = Missing, _steps = []}]}]},
+            Criteria {_criteriaName = "AC: A succesful genesis ceremony", _testName = "test: [todo: requires integration test]", _status = Missing, _steps = [
+              Step "ceremonyMaster is instatantied with flags --required-sigs 2 --duration 5min --interval 10sec --bonds-file <holds two nodes validatorA and validatorB.",
+              Step "validatorA and validatorB joins p2p, both poining ceremonyMaster as bootstrap",
+              Step "ceremonyMaster sends UnapprovedBlock to validatorA and validatorB",
+              Step "validatorA and validatorB receives UnapprovedBlock",
+              Step "validatorA and validatorB send back BlockApproval",
+              Step "ceremonyMaster transitions to ApprovedBlockReceivedHandler",
+              Step "ceremonyMaster sends ApprovedBlock to validatorA and validatorB",
+              Step "validatorA and validatorB transition to ApprovedBlockReceivedHandler",
+              Step "ceremonyMaster, validatorA and validatorB tip points to block (genesis) where it has no parent and Bonds holds validatorA and validatorB"]},
+            Criteria {_criteriaName = "AC: A succesful genesis ceremony with read-only nodes joining", _testName = "test: [todo: requires integration test]", _status = Missing, _steps = [
+              Step "ceremonyMaster is instatantied with flags --required-sigs 2 --duration 5min --interval 10sec --bonds-file <holds two nodes validatorA and validatorB.",
+              Step "validatorA and validatorB joins p2p, both poining ceremonyMaster as bootstrap",
+              Step "readOnlyA(read-only) joins p2p",
+              Step "ceremonyMaster sends UnapprovedBlock to validatorA and validatorB",
+              Step "validatorA and validatorB receives UnapprovedBlock",
+              Step "validatorA and validatorB send back BlockApproval",
+              Step "ceremonyMaster transitions to ApprovedBlockReceivedHandler",
+              Step "ceremonyMaster sends ApprovedBlock to validatorA and validatorB",
+              Step "validatorA and validatorB transition to ApprovedBlockReceivedHandler",
+              Step "ceremonyMaster, validatorA and validatorB tip points to block (genesis) where it has no parent and Bonds holds validatorA and validatorB",
+              Step "readOnlyA Strong [Str \"never\"] transitions to ApprovedBlockReceivedHandler"]},
+            Criteria {_criteriaName = "AC: A NOT succesful genesis ceremony (not enough sigs)", _testName = "test: [todo: requires integration test]", _status = Missing, _steps = [
+              Step "ceremonyMaster is instatantied with flags --required-sigs 3 --duration 5min --interval 10sec --bonds-file <holds two nodes validatorA and validatorB.",
+              Step "validatorA and validatorB joins p2p, both poining ceremonyMaster as bootstrap",
+              Step "ceremonyMaster sends UnapprovedBlock to validatorA and validatorB",
+              Step "validatorA and validatorB receives UnapprovedBlock",
+              Step "validatorA and validatorB send back BlockApproval",
+              Step "ceremonyMaster logs an error about not getting enough signatures on time (duration)"]},
+            Criteria {_criteriaName = "AC: A validator catching up after ceremony", _testName = "test: [todo: requires integration test]", _status = Missing, _steps = [
+              Step "genesis reach as described in A succesful genesis ceremony",
+              Step "validatorC joins p2p, pointing on ceremonyMaster as bootstrap",
+              Step "validatorC sends ApprobedBlockRequest to ceremonyMaster",
+              Step "ceremonyMaster sends ApprovedBlock to validatorC",
+              Step "validatorC transitions to ApprovedBlockReceivedHandler",
+              Step "validatorC tip points to block (genesis) where it has no parent and Bonds holds validatorA and validatorB"]}]}]},
         Feature {_featureName = "Proof of stake consensus", _userStories = [
           UserStory {_userStoryDesc = "UC: As a dApp developer I want to be able to deploy my rholang contract to a validator", _criteria = [
-            Criteria {_criteriaName = "AC: A correct contract gets deployed successfully", _testName = "test: [todo: requires integration test]", _status = Missing, _steps = []},
-            Criteria {_criteriaName = "AC: An incorrect contract does not get deployed", _testName = "test: [todo: requires integration test]", _status = Missing, _steps = []}]}]}]}
+            Criteria {_criteriaName = "AC: A correct contract gets deployed successfully", _testName = "test: [todo: requires integration test]", _status = Missing, _steps = [
+              Step "instantiate p2p network with single ceremonyMaster that transitions to ApprovedBlockReceivedhandler (--required-sig 0)",
+              Step "call rnode deploy with rholang/examples/tut-philosophers.rho on ceremonyMaster",
+              Step "assert a success on std out",
+              Step "rnode deploy exit code should be 0"]},
+            Criteria {_criteriaName = "AC: An incorrect contract does not get deployed", _testName = "test: [todo: requires integration test]", _status = Missing, _steps = [
+              Step "instantiate p2p network with single ceremonyMaster that transitions to ApprovedBlockReceivedhandler (--required-sig 0)",
+              Step "call rnode deploy with invalid contract on ceremonyMaster",
+              Step "assert a error logs on std out",
+              Step "rnode deploy exit code should be 1"]}]},
+          UserStory {_userStoryDesc = "UC: No AC!", _criteria = []}]}]}
