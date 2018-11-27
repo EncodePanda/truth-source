ca2a8f7c51f2e9bf78f66f3453c18589bafc9450
Pawel Szulc <paul.szulc@gmail.com>
2018-11-27 14:22:21 +0100

    Add failing markdown scenario

--- test/FeaturesRaw.hs
+++ test/FeaturesRaw.hs
@@ -91,3 +91,90 @@ sample = [r|
 * just for testing purposes
 
 |]
+
+sample2 = [r|
+# Mainnet Feature Requirements
+
+## Peer to Peer Network
+### As a Node operator, I want to be able to bootstrap to the network by connecting to any known node
+### As a Node operator, once connected via a bootstrap node, I want to discover and connect to peers
+### As a Node operator, I want to know how many peers I am connected to
+## Network Launch
+### As a Coop SRE I want to launch a network
+#### A succesful genesis ceremony 
+
+##### test: test/test_genesis_ceremony.py::test_successful_genesis_ceremony 
+##### steps:
+
+* `ceremonyMaster` is instatantied with flags `--required-sigs 2 --duration 5min --interval 10sec --bonds-file <holds two nodes validatorA and validatorB`.
+* `validatorA` and `validatorB` joins p2p, both pointing to `ceremonyMaster` as bootstrap
+* `ceremonyMaster` sends `UnapprovedBlock` to `validatorA` and `validatorB`
+* `validatorA` and `validatorB` receives `UnapprovedBlock`
+* `validatorA` and `validatorB` send back `BlockApproval`
+* `ceremonyMaster` transitions to `ApprovedBlockReceivedHandler`
+* `ceremonyMaster` sends `ApprovedBlock` to `validatorA` and `validatorB`
+* `validatorA` and `validatorB` transition to ApprovedBlockReceivedHandler
+* `ceremonyMaster`, `validatorA` and `validatorB` tip points to block (genesis) where it has no parent and Bonds holds `validatorA` and `validatorB`
+
+#### A succesful genesis ceremony with read-only nodes joining 
+##### test: not available
+##### steps:
+
+* `ceremonyMaster` is instatantied with flags `--required-sigs 2 --duration 5min --interval 10sec --bonds-file <holds two nodes validatorA and validatorB`.
+* `validatorA` and `validatorB` joins p2p, both pointing to `ceremonyMaster` as bootstrap
+* `readOnlyA`(read-only) joins p2p
+* `ceremonyMaster` sends `UnapprovedBlock` to `validatorA` and `validatorB`
+* `validatorA` and `validatorB` receives `UnapprovedBlock`
+* `validatorA` and `validatorB` send back `BlockApproval`
+* `ceremonyMaster` transitions to `ApprovedBlockReceivedHandler`
+* `ceremonyMaster` sends `ApprovedBlock` to `validatorA` and `validatorB`
+* `validatorA` and `validatorB` transition to ApprovedBlockReceivedHandler
+* `ceremonyMaster`, `validatorA` and `validatorB` tip points to block (genesis) where it has no parent and Bonds holds `validatorA` and `validatorB`
+* `readOnlyA` **never** transitions to `ApprovedBlockReceivedHandler`
+
+
+#### A NOT succesful genesis ceremony (not enough sigs)
+##### test: not available
+##### steps:
+
+* `ceremonyMaster` is instatantied with flags `--required-sigs 3 --duration 5min --interval 10sec --bonds-file <holds two nodes validatorA and validatorB`.
+* `validatorA` and `validatorB` joins p2p, both pointing to `ceremonyMaster` as bootstrap
+* `ceremonyMaster` sends `UnapprovedBlock` to `validatorA` and `validatorB`
+* `validatorA` and `validatorB` receives `UnapprovedBlock`
+* `validatorA` and `validatorB` send back `BlockApproval`
+* `ceremonyMaster` logs an error about not getting enough signatures on time (`duration`)
+
+#### A validator catching up after ceremony
+##### test: not available
+##### steps:
+
+* genesis reached as described in "A succesful genesis ceremony"
+* `validatorC` joins p2p, pointing on `ceremonyMaster` as bootstrap
+* `validatorC` sends `ApprovedBlockRequest` to `ceremonyMaster`
+* `ceremonyMaster` sends `ApprovedBlock` to `validatorC`
+* `validatorC` transitions to `ApprovedBlockReceivedHandler`
+* `validatorC` tip points to block (genesis) where it has no parent and Bonds holds `validatorA` and `validatorB`
+
+## Bonding/Unbonding
+### As a Node Validator, I want to be able to add my stake to the network and be recognized as a validator so I can participate in proof of stake consensus and be eligible to earn rewards (validating)
+### As a Node Validator, I want to be able to retrieve my stake from the network and no longer be recognized a as validator
+## Proof of stake consensus
+### As a dApp developer I want to be able to deploy my rholang contract to a validator
+#### A correct contract gets deployed successfully
+##### test: not available
+##### steps:
+
+* instantiate p2p network with single `ceremonyMaster` that transitions to `ApprovedBlockReceivedhandler` (`--required-sig 0`)
+* call `rnode deploy` with `rholang/examples/tut-philosophers.rho` on `ceremonyMaster`
+* assert a success on std out
+* `rnode deploy` exit code should be 0
+
+#### An incorrect  contract does not get deployed 
+##### test: not available
+##### steps:
+
+* instantiate p2p network with single `ceremonyMaster` that transitions to `ApprovedBlockReceivedhandler` (`--required-sig 0`)
+* call `rnode deploy` with invalid contract on `ceremonyMaster`
+* assert a error logs on std out
+* `rnode deploy` exit code should be 1
+|]
